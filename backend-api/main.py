0a9508befd6dfb85783c7c513234e305dc0d1dd6
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-03-15 16:16:36 -0400

    wire db models

--- backend-api/main.py
+++ backend-api/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from database import engine
+import models
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
 
 @app.get("/health")
 def health_check():
