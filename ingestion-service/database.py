48c63f79082ca25d56a4c9569fc42a25fbde377b
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-04-02 12:23:52 -0400

    copied over db and models to ingestion service

---
+++ ingestion-service/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/rocketlaunch")
+
+engine = create_engine(DATABASE_URL) ##acts as the primary source of connectivity to a database.
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) ##a factory for creating DB sessions
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ ingestion-service/models.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.sql import func
+from database import Base
+from sqlalchemy.orm import relationship
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(75), unique=True, nullable=False, index=True)
+    phone_number = Column(String(20), nullable=True)
+    password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    subscriptions = relationship('Subscription', back_populates='user')
+
+class Launch(Base):
+    __tablename__ = 'launches'
+
+    id = Column(Integer, primary_key=True, index=True)
+    external_id = Column(String(100), unique=True, nullable=False)
+    name = Column(String(200), nullable=False)
+    agency = Column(String(100), nullable=False)
+    status = Column(String(50), nullable=False)
+    net = Column(DateTime(timezone=True), nullable=True)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+    subscriptions = relationship('Subscription', back_populates='launch')
+
+
+class Subscription(Base):
+    __tablename__ = 'subscriptions'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    launch_id = Column(Integer, ForeignKey('launches.id'), nullable=True)
+    agency = Column(String(100), nullable=True)
+    notify_email = Column(Boolean, default=True, nullable=False)
+    notify_phone = Column(Boolean, default=False, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    user = relationship('User', back_populates='subscriptions')
+    launch = relationship('Launch', back_populates="subscriptions")
+
+    
+
