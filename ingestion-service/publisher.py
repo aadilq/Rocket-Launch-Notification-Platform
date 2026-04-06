0b35edf5f6ce8a0ca34da55df7b2192ca4899431
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-04-06 13:22:26 -0400

    set up RabbitMQ as message broker

---
+++ ingestion-service/publisher.py
@@ -0,0 +1,23 @@
+import pika
+import json
+import os
+
+RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
+
+def publish_event(event):
+    connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
+    channel = connection.channel()
+
+    channel.queue_declare(queue="launch_events", durable=True)
+
+    channel.basic_publish(
+        exchange="",
+        routing_key="launch_events", 
+        body=json.dump(event),
+        properties=pika.BasicProperties(
+            delivery_mode=2
+        )
+    )
+    print(f"Published event: {event['event_type']} - {event['launch_name']}")
+    connection.close()
+
