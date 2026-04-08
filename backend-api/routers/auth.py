ad149970b5c5477961b27adf338b2f40a994e85a
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-04-08 16:21:50 -0400

    routes for register and login

---
+++ backend-api/routers/auth.py
@@ -0,0 +1,64 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
+from models import User
+from passlib.context import CryptContext
+from jose import jwt
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+import os
+
+router = APIRouter()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") ##Handles password hashing & verification
+
+SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+class RegisterRequest(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+
+def create_access_token(data: dict):
+    data_to_encode = data.copy()
+    expire = datetime.utc()+ timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data_to_encode.update({"exp": expire})
+    return jwt.encode(data_to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+@router.post("/auth/register")
+def register(request: RegisterRequest, db: Session = Depends(get_db)):
+    existing_user = db.query().filter(User.email == request.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    hashed_password = pwd_context.hash(request.password)
+
+    new_user = User(
+        name = request.name,
+        email = request.email,
+        password_hash = hashed_password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return {"message": "User registered successfully!"}
+
+
+@router.post("/auth/login")
+def login(request: LoginRequest, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == request.email).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    if not pwd_context.verify(request.password, user.password_hash):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    
+    token = create_access_token({"sub": str(user.id)})
+    return {"access token": token, "token_type": "bearer"}
+
