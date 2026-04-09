008e6aefcda4320ae6cc69b502baf1d2772d6385
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-04-09 12:35:45 -0400

    routes for launches

--- backend-api/routers/launches.py
+++ backend-api/routers/launches.py
@@ -1,1 +1,22 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+from models import Launch
+
+router = APIRouter()
+
+@router.get("/launches")
+def get_launches(db: Session = Depends(get_db)):
+    launches = db.query(Launch).order_by(Launch.net.asc.all())
+
+    return [
+        {
+            "id": launch.id,
+            "external_id": launch.external_id,
+            "name": launch.name,
+            "agency": launch.agency,
+            "status": launch.status,
+            "net": launch.net
+        }
+        for launch in launches
+    ]
