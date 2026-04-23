206c5b379f716065ef8037e3dc94aebe9be30cab
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-04-23 19:31:10 -0400

    add authFetch function

---
+++ frontend/frontend-NASA_proj/src/utils/api.js
@@ -0,0 +1,19 @@
+ export async function authFetch(url, options = {}) {
+      const token = localStorage.getItem("token")
+                
+      const res = await fetch(url, {
+          ...options,
+          headers: {                                                                
+              ...options.headers,                                    
+              "Authorization": `Bearer ${token}`
+          }              
+      })
+                                                                                       
+      if (res.status === 401) {                
+          localStorage.removeItem("token")
+          window.location.href = "/login"                                           
+          return                       
+      }
+                                        
+      return res                
+  }                      
