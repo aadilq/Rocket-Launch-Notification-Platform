d8c2ace54d626f8842f498107462f2f686ae38c1
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-04-22 22:34:48 -0400

    query future launches

--- backend-api/routers/launches.py
+++ backend-api/routers/launches.py
@@ -2,12 +2,15 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
 from models import Launch
+from datetime import datetime, timezone
 
 router = APIRouter()
 
 @router.get("/launches")
 def get_launches(db: Session = Depends(get_db)):
     launches = db.query(Launch).order_by(Launch.net.asc()).all()
+    now = datetime.now(timezone.utc)
+    launches = db.query(Launch).filter(Launch.net > now).order_by(Launch.net.asc()).all()
 
     return [
         {
