af446a5de2d1a79cd608b81c2d7c68ca1f24f5e2
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-05-01 17:01:27 -0400

    fix sql typo

--- backend-api/database.py
+++ backend-api/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/rocketlaunch")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@localhost:5432/rocketlaunch").replace("postgres://", "postgresql://")
 
 engine = create_engine(DATABASE_URL) ##acts as the primary source of connectivity to a database.
 
